79ae54b6a75c33c18f933b13dcd94c4e1f340221
Art <art-75@yandex.ru>
2023-09-10 12:36:16 +0300

    HW_5_2_final

--- HW_5_2.py
+++ HW_5_2.py
@@ -3,9 +3,22 @@
 # с именем в качестве ключа и суммой премии в качестве значения. 
 # Сумма рассчитывается как ставка умноженная на процент премии.
 
-employee = ['Boris', 'Peter', 'Sergio']
-rate = [1300, 3100, 5200]
-bonus = ['14%', '18%', '21%']
+Employee = ["Boris", "Aleksey", "David", "Leo"]
+Rate = [10000, 20000, 15000, 30000]
+Bonus = ["10.25%", "7.14%", "11.50%", "14.65%"]
 
-dictionary = {employee: rate * float(bonus.strip('%')) / 100 for employee, rate, bonus in zip(employee, rate, bonus)}
-print(dictionary)
+
+def gen_dict(employee: list[str], rate: list[int], bonus: list[str]):
+    yield {d[0]: d[1] for d in
+           list(map(lambda y: (y[0], y[1] * y[2] / 100), zip(employee, rate, map(lambda x: float(x[:-1]), bonus))))}
+
+
+def main():
+    print(Employee)
+    print(Rate)
+    print(Bonus)
+    print(*gen_dict(Employee, Rate, Bonus))
+
+
+if __name__ == "__main__":
+    main()
